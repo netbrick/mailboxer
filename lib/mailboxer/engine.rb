d96a580030f2308a1cbf8010c9b57f37053a9fc1
Tomáš Fiala <tomas.fiala@netbrick.eu>
2015-08-13 15:45:22 +0200

    add migration init

--- lib/mailboxer/engine.rb
+++ lib/mailboxer/engine.rb
@@ -8,9 +8,18 @@ end
 
 module Mailboxer
   class Engine < Rails::Engine
+    isolate_namespace EngineWithMigrations
     initializer "mailboxer.models.messageable" do
       ActiveSupport.on_load(:active_record) do
         extend Mailboxer::Models::Messageable::ActiveRecordExtension
+      end
+    end
+
+    initializer :append_migrations do |app|
+      unless app.root.to_s.match root.to_s
+        config.paths["db/migrate"].expanded.each do |expanded_path|
+          app.config.paths["db/migrate"] << expanded_path
+        end
       end
     end
   end
